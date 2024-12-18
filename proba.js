1969db760564ba20f8bb6dc07bf6fbfdb886cdba
Ilya Kravtsov <krawzow1@yandex.ru>
2024-12-18 20:52:35 +0300

    Интерполяция, массив - частн случ объекта

---
+++ proba.js
@@ -0,0 +1,36 @@
+// let a = 5;
+// console.log(a);
+
+// //создание объекта
+// const obj  = {
+//     name: 'Ilya',
+//     age: '26',
+//     country: 'Russia'
+// };
+// //доступ к элементу объекта
+// console.log(obj.name);
+// //или
+// console.log(obj["country"]);
+
+// const arr = [1,2,3]; //массив
+// const obj1 = {a: 1, b: 2}; //объект
+// obj1.c = 'ky'; //создание нового свой-ва объекта
+// console.log(obj1['c']);
+// console.log(obj1.c);
+
+// arr[10] = '23344'; //в массив, состоящий из 3-х элементов
+// // добавляю сразу 10-й эл. Это приводит к тому, что 
+// // с 3 ро 9-й индекс будут undefined элементы
+// console.log(arr);
+///alert('FDFD');
+//const question = confirm('Ты гей?') // форма с ответом Да/Нет, который можно сохранить
+//alert(question)
+//console.log(question)
+
+// const answer = prompt('То, что введешь сейчас - будет строкой','123')
+// const answerint = +prompt('То, что введешь сейчас - будет числом','321')
+// console.log(typeof(answer), typeof(answerint))
+
+// //Интерполяция
+// console.log(`Привет, вот число ${a}`)
+
