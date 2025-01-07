777cfc15ec5472beefc22b5d15fa32caa66c0301
Ilya Kravtsov <krawzow1@yandex.ru>
2025-01-07 21:49:08 +0300

    решены задачи 2-38

--- 2-38-2025/js/script.js
+++ 2-38-2025/js/script.js
@@ -24,3 +24,33 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list');
+        
+adv.forEach(i => {
+    i.remove()
+})
+genre.textContent = 'драма';
+
+// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+// Реализовать только при помощи JS
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+// Отсортировать их по алфавиту
+// movieList.forEach(i => {          //не робит, потому что movieList одиночный эл, а не коллекция
+//     i.remove()
+// })
+movieList.innerHTML = ""; // Очистка текущего списка
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
